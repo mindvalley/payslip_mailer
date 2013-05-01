fdbff03e3fbc7d72fb9b0830d200e0a54d29d3a7
Tristan <tristan.gomez@gmail.com>
2013-05-01 17:16:41 +0800

    add mailer config for production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,9 @@ PayslipMailer::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+  config.action_mailer.delivery_method = :sendmail
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = false
+
 end
