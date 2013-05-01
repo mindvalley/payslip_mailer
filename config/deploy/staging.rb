c57b5f9a7377ee849362101541740157f6aa5def
Tristan <tristan.gomez@gmail.com>
2013-05-01 16:46:07 +0800

    use correct server name

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,5 +1,5 @@
 
 server 'daily.mvstg.com', :web, :app, :db, primary: true
 set :port, 20022
-set :server_name, 'daily.mvstg.com'
+set :server_name, 'payslip_mailer.mvstg.com'
 set :branch, 'master'
