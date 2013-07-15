bfebfb8b56bc519a0ca05b21a80db6cf485519f2
Muhammad Furqan Asghar <furqan@mindvalley.com>
2013-07-15 16:21:11 +0800

    added root path

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ PayslipMailer::Application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root to: 'welcome#index'
+  root to: 'payslips#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
